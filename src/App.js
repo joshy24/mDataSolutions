cf9e0de1be116ca6c67fff1c2941a839877f8201
Tonye Greene <35413072+ellysmoore@users.noreply.github.com>
2021-11-10 11:16:26 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from "react";
-import {  BrowserRouter as Router, Route, Routes  } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import OurApproach from "./Components/about/OurApproach";
 import Footer from "./Components/Footer";
 import { Header } from "./Components/Header";
 import Service from "./Components/Service";
 import About from "./Pages/About";
 import Home from "./Pages/Home";
 
-function App () {
+function App() {
   return (
     <div id="main-wrapper" class="main-wrapper">
       <Router>
         <Header />
-          <Routes>
+        <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
+          {/* about pages  */}
+          <Route exact path="/OurApproach" element={<OurApproach />} />
+
+          {/*end of about pages  */}
+
           <Route exact path="/services" element={<Service />} />
-          </Routes>
+        </Routes>
         <Footer />
       </Router>
     </div>
   );
-};
+}
 
 export default App;

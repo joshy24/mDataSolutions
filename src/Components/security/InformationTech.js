0152feaf2c5a09f0d638b788b1f78323ea3180b3
Tonye Greene <35413072+ellysmoore@users.noreply.github.com>
2021-11-17 12:55:05 +0100

    commit

---
+++ src/Components/security/InformationTech.js
@@ -0,0 +1,91 @@
+import React, { useEffect } from "react";
+import sal from "sal.js";
+import business from '../../images/business.png'
+
+const InformationTech = () => {
+  useEffect(() => {
+    sal();
+  }, []);
+  return (
+    <>
+      <div class="breadcrum-area breadcrumb-banner single-breadcrumb">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6">
+              <div
+                class="section-heading heading-left"
+                data-sal="slide-right"
+                data-sal-duration="1000"
+                data-sal-delay="300"
+              >
+                <h1 class="title h2">Information Tech</h1>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div
+                class="banner-thumbnail"
+                data-sal="slide-up"
+                data-sal-duration="1000"
+                data-sal-delay="400"
+              >
+                <img
+                  class="paralax-image"
+                  src={business}
+                  alt="Illustration"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <ul class="shape-group-8 list-unstyled">
+          <li
+            class="shape shape-1"
+            data-sal="slide-right"
+            data-sal-duration="500"
+            data-sal-delay="100"
+          >
+            <img src="assets/media/others/bubble-9.png" alt="Bubble" />
+          </li>
+          <li
+            class="shape shape-2"
+            data-sal="slide-left"
+            data-sal-duration="500"
+            data-sal-delay="200"
+          >
+            <img src="assets/media/others/bubble-20.png" alt="Bubble" />
+          </li>
+          <li
+            class="shape shape-3"
+            data-sal="slide-up"
+            data-sal-duration="500"
+            data-sal-delay="300"
+          >
+            <img src="assets/media/others/line-4.png" alt="Line" />
+          </li>
+        </ul>
+      </div>
+
+      <section class="section-padding single-portfolio-area">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-5">
+              <div class="section-heading heading-left mb-0">
+                {/* <span class="subtitle">Software, Technology</span> */}
+                <h3 class="title">Information  Tech</h3>
+              </div>
+              <p>
+                Repetitive and boring tasks can be distracting from primary
+                responsibilities and obligations, with MData’s BPA solutions,
+                businesses become more efficient and productive while work gets
+                more interesting for employees. The software simply automates
+                the tasks that are time-consuming.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default InformationTech;
